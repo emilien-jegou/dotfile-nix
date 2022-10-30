516a7ed8216c70e1ac5e19c359c5f256ac495c75
emilien <emj339@gmail.com>
2022-10-30 22:28:33 +0100

    fix(tmux): broken tpm plugin manager
    The config for tpm was never download as such we received errors on tmux
    launch and plugins were inactive.

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -1,13 +1,21 @@
 { inputs, lib, config, pkgs, ... }:
 with lib;
 let
-    cfg = config.modules.tmux;
+  cfg = config.modules.tmux;
 in {
-    options.modules.tmux = { enable = mkEnableOption "tmux"; };
+  options.modules.tmux = { enable = mkEnableOption "tmux"; };
 
-    config = mkIf cfg.enable {
-        home.packages = with pkgs; [ tmux xclip ];
-
-        home.file.".tmux.conf".source = ./tmux.conf;
+  config = mkIf cfg.enable {
+    home.file = { ".tmux/plugins/tpm".source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "v3.0.0";
+      sha256 = "qYBMDLIEkgiTFxjlF8AHn31HZ4nt/ZoeerzX70SSBaM=";
     };
-}
+  };
+
+  home.packages = with pkgs; [ tmux xclip ];
+
+  home.file.".tmux.conf".source = ./tmux.conf;
+};
+  }
