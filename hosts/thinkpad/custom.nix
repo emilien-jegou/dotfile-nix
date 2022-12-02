88958991ef59057ede76ae24b027e0e583e45f4e
Emilien <emj339@gmail.com>
2022-12-02 17:46:40 +0100

    fixup! feat: 02/12/2022

---
+++ hosts/thinkpad/custom.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.defaultSession = "plasma";
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    khelpcenter
+    oxygen
+    print-manager
+    konsole
+  ];
+
+  environment.systemPackages = with pkgs; [
+    slack
+    openssl
+    jq
+    bash
+    shellcheck
+    obsidian
+    discord
+    postman
+    hamster
+    chromium
+    mailspring
+    dbeaver
+    yarn
+    php
+    php81Packages.composer
+    gnome.gedit
+    peek
+  ];
+
+  environment.variables = { "QT_STYLE_OVERRIDE" = "kvantum"; };
+
+  # Install fonts
+  fonts = {
+    fonts = with pkgs; [
+      jetbrains-mono
+      roboto
+      openmoji-color
+      (nerdfonts.override { fonts = [ "JetBrainsMono" "Iosevka" ]; })
+    ];
+
+    fontconfig = {
+      hinting.autohint = true;
+      defaultFonts = {
+        emoji = [ "OpenMoji Color" ];
+      };
+    };
+  };
+
+  networking.extraHosts =
+  ''
+    127.0.0.1	platform.local
+    127.0.0.1	operator.platform.local
+  '';
+
+  programs.zsh.enable = true;
+  programs.dconf.enable = true;
+}
---
+++ hosts/thinkpad/user.nix
@@ -0,0 +1,30 @@
+{ config, lib, inputs, ...}:
+
+{
+    imports = [ ../../modules/default.nix ];
+
+    config.modules = {
+      # desktop
+      plasma-config.enable = true;
+      latte-dock.enable = true;
+
+      # gui
+      firefox.enable = true;
+      termite.enable = true;
+      alacritty.enable = true;
+      tmux.enable = true;
+      tig.enable = true;
+
+      # cli
+      nvim.enable = true;
+      lf.enable = true;
+      zsh.enable = true;
+      git.enable = true;
+      gpg.enable = true;
+      direnv.enable = true;
+
+      # system
+      xdg.enable = true;
+      packages.enable = true;
+    };
+}
